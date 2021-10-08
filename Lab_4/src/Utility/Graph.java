38958d2b20e77abbad1255a01719ff4dc5482a90
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2021-10-08 03:20:44 +0200

    Transfer

--- Lab_4/src/Utility/Graph.java
+++ Lab_4/src/Utility/Graph.java
@@ -12,32 +12,67 @@ import edu.princeton.cs.algs4.Bag;
 * This class is an utility class that implements an undirected graph. 
 * It implements an API based on the description in 
 * <a href="https://algs4.cs.princeton.edu/41graph/">Princeton</a>.
-* The class supports operations such as adding edges to the graph, iteration 
-* over vertices adjacent to a vertex and returning number of edges and vertices.
-* 
-* 
-* Based on:
-* <a href="https://algs4.cs.princeton.edu/41graph/Graph.java.html">Link</a>
+ The class supports operations such as adding edges to the graph, iteration 
+ over vertices adjacentVertices to a vertex and returning number of edges and vertices.
+ 
+ 
+ Based on:
+ <a href="https://algs4.cs.princeton.edu/41graph/Graph.java.html">Link</a>
 *
 *******************************************************************************/
 
 
 public class Graph {
+    private final int vertices;
+    private int edges;
+    private Bag<Integer>[] adjacentVertices;
     
-    // Default constructor
-    public Graph() {
-        Bag bag = new Bag();
+    
+    // Initialize a graph with V vertices 
+    public Graph(int V) {
+        this.vertices = V;
+        this.edges = 0;
+        
+        adjacentVertices = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adjacentVertices[v] = new Bag<Integer>();
+        }
+    }
+    
+    /**
+     * Adds an edge between vertex A and vertex B to the graph.
+     * 
+     * @param vertex_A First vertex
+     * @param vertex_B 
+     */
+    public void addEdge(int vertex_A, int vertex_B) {
         
     }
     
-    // Add edge
+    
+    /**
+     * Returns the vertices adjacent to the input vertex.
+     *
+     * @param  vertex the vertex
+     * @return the vertices adjacent to the input vertex as an iterable
+     */
+    public Iterable<Integer> adjacentVertices(int vertex) {
+        return adjacentVertices[vertex];
+    }
+    
+    /**
+     * Prints out the graph.
+     * For debugging purpose.
+     */
+    public void graphToString() {
+        
+    }
     
     /**
      * Main method with unit testing for the class.
      * @param args takes no input argument
      */
     public static void main(String[] args) {
-        Graph graph = new Graph();
         
         // Test methods
     }
---
+++ Lab_4/src/Utility/SymbolGraph.java
@@ -0,0 +1,30 @@
+package Utility;
+
+/*********************************** README ************************************
+*
+* Seminar 4 - Symbol Graph
+* @author Alexander Lundqvist
+* Created: 07-10-2021
+*
+* About this class:
+*
+*
+* Based on:
+* <a href="https://algs4.cs.princeton.edu/41graph/SymbolGraph.java.html">Link</a>
+*
+*******************************************************************************/
+
+public class SymbolGraph {
+    
+    // Default constructor
+    public SymbolGraph() {
+    }
+
+    /**
+     * Main method with unit testing for the class.
+     * @param args takes no input arguments
+     */    
+    public static void main(String[] args) {
+        
+    }
+}
