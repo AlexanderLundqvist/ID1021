dfadccc3a4ad97bff70146cd6c01ab2c9827cfa0
Alexander Lundqvist <alexander.lundqvist@outlook.com>
2021-11-05 15:13:57 +0100

    Backup
    Weird stuff on laptop, backup in case of code deletion

--- Lab_FX/src/Task_3/Task_3.java
+++ Lab_FX/src/Task_3/Task_3.java
@@ -2,12 +2,14 @@ package Task_3;
 
 /*********************************** README ************************************
 *
-* Title placeholder
+* Seminar FX - Sorting tuples
 * @author Alexander Lundqvist
 * Created: 07-10-2021
 *
 * About this class:
-*
+* This class provides methods for sorting a list of <string, integer> pairs in
+* both alphabetical and ascending order. The sorting algorithm implemented is
+* a simple merge sort.
 *
 * Based on:
 * <a href="">Link</a>
@@ -38,17 +40,70 @@ package Task_3;
 * 
 *******************************************************************************/
 
-public class Task_3 {
+public class Task_3<Pair extends Comparable<Pair>> {
+    private final int amount;
+    private String[] strings;
+    private int[] integers;
+    private Pair[] pairs;
+    private int size;
     
     // Default constructor
-    public Task_3() {
+    public Task_3(int amount) {
+        this.amount = amount;
+        this.pairs = (Pair[]) new Comparable[amount];
+        this.size = 0;
     }
-
+    
+    private class Pair {
+        private String string;
+        private int integer;
+        
+        public Pair(String string, int integer) {
+            this.string = string;
+            this.integer = integer;
+        }
+        
+        public void print() {
+            System.out.println(this.string + " " + this.integer);
+        }
+    }
+    
+    public void addPair(String string, int integer) {
+        pairs[size] = new Pair(string, integer);
+        size++;
+    }
+    
+    public int size() {
+        return this.size;
+    }
+    
+    public void sort() {
+        
+    }
+    
+    public void printList() {
+        
+    }
+    
     /**
      * Main method with unit testing for the class.
      * @param args takes no input arguments
+     * 
+     * B 47
+     * K 97
+     * A 12
+     * B 567
+     * A 32
+     * K 4
      */    
     public static void main(String[] args) {
+        Task_3 test = new Task_3(6);
         
+        test.addPair("B", 47);
+        test.addPair("K", 97);
+        test.addPair("A", 12);
+        test.addPair("B", 567);
+        test.addPair("A", 32);
+        test.addPair("K", 4);
     }
 }
---
+++ Lab_FX/src/Utility/RandomKeyValue.java
@@ -0,0 +1,41 @@
+package Utility;
+
+/*********************************** README ************************************
+*
+* Seminar FX - Random Key-value pair generator
+* @author Alexander Lundqvist
+* Created: 04-11-2021
+*
+* About this class:
+* This class implements creating random key-value pairs, key is in the format 
+* of strings and the values are simple integers. The class supports creating of
+* singular key-value pairs but also creating multiple key value-pairs at the
+* same time.
+*
+* Based on:
+* <a href="">Link</a>
+*
+*******************************************************************************/
+
+public class RandomKeyValue {
+    
+    // Default constructor. Initializes an instance of the randomomizer
+    public RandomKeyValue() {
+    }
+    
+    public int[] randomizeInt(int seed) {
+        return null;
+    }
+    
+    public String[] randomizeString(int seed) {
+        return null;
+    }
+    
+    /**
+     * Contains unit testing for the class.
+     * @param args takes no input arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
